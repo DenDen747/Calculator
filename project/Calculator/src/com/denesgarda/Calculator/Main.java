b328e5968d9266cd5a24d60636e3eb9ea8f372cc
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2022-01-09 16:37:15 -0800

    Built advanced menu

--- project/Calculator/src/com/denesgarda/Calculator/Main.java
+++ project/Calculator/src/com/denesgarda/Calculator/Main.java
@@ -5,9 +5,15 @@ public class Main {
 
     public static double VERSION = 1.0;
     public static int inputID = 0;
-    public static String breaker = "..................................................";
+    public static String breaker = "......................................................................";
 
-    public static String menuMessage = "---Calculator v" + VERSION + "---\n[A] Basic computation\n[B] Variable identification\n[C] Usage\n[~] Exit";
+    public static String adv = "";
+
+    public static String menuMessage = "---Calculator v" + VERSION + "---\n" +
+            "[A] Basic computation\n" +
+            "[B] Advanced computation\n" +
+            "[C] Usage\n" +
+            "[~] Exit";
     public static String usage = "Usage:\n" +
             "- Basic computation\n" +
             "    - `+` Addition\n" +
@@ -64,9 +70,9 @@ public class Main {
                 inputID = 2;
             } else if (input.equalsIgnoreCase("B")) {
                 printBreaker();
-                System.out.println("[THIS MODE IS NOT YET AVAILABLE]");
-                printBreaker();
-                System.out.println(menuMessage);
+                System.out.println("---Advanced computation---\n[~] Exit");
+                window.setTitle("Calculator v" + VERSION + " - Advanced computation");
+                inputID = 4;
             } else if (input.equalsIgnoreCase("C")) {
                 printBreaker();
                 System.out.println(usage);
@@ -112,6 +118,69 @@ public class Main {
             System.out.println(menuMessage);
             window.setTitle("Calculator v" + VERSION + " - Menu");
             inputID = 1;
+        } else if (inputID == 4) {
+            if (input.equalsIgnoreCase("~")) {
+                printBreaker();
+                System.out.println(menuMessage);
+                window.setTitle("Calculator v" + VERSION + " - Menu");
+                inputID = 1;
+            } else {
+                adv = input;
+                System.out.println("\n[1] Solve\n" +
+                        "[2] Solve for variables\n" +
+                        "[3] Simplify\n" +
+                        "[4] Graph\n" +
+                        "[~] Cancel");
+                inputID = 5;
+            }
+        } else if (inputID == 5) {
+            if (input.equalsIgnoreCase("1")) {
+                String ans = String.valueOf(Logic.compute(adv));
+                if (ans.equalsIgnoreCase("Infinity/NaN")) {
+                    ans = "MATH ERROR";
+                }
+                if (ans.contains(".0/")) {
+                    ans = ans.replace(".0/", "/");
+                }
+                if (ans.endsWith(".0")) {
+                    ans = ans.substring(0, ans.length() - 2);
+                }
+                if (ans.contains("/")) {
+                    if (ans.split("/")[1].equals("1")) {
+                        ans = ans.split("/")[0];
+                    } else if (ans.split("/")[1].equals("0")) {
+                        ans = "Undefined";
+                    }
+                }
+                printEdge(ans);
+                printBreaker();
+                inputID = 4;
+            } else if (input.equalsIgnoreCase("2")) {
+                String ans = "Not yet implemented";
+                printEdge(ans);
+                printBreaker();
+                inputID = 4;
+            } else if (input.equalsIgnoreCase("3")) {
+                String ans = "Not yet implemented";
+                printEdge(ans);
+                printBreaker();
+                inputID = 4;
+            } else if (input.equalsIgnoreCase("4")) {
+                String ans = "Not yet implemented";
+                printEdge(ans);
+                printBreaker();
+                inputID = 4;
+            } else if (input.equalsIgnoreCase("~")) {
+                String ans = "Canceled";
+                printEdge(ans);
+                printBreaker();
+                inputID = 4;
+            } else {
+                String ans = "Canceled";
+                printEdge(ans);
+                printBreaker();
+                inputID = 4;
+            }
         }
     }
 }
